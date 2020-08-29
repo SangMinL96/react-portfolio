3fc31aa975aae01e58b69d4af2eca09138aa8040
SangMinL96 <59109788+SangMinL96@users.noreply.github.com>
2020-08-29 22:58:29 +0900

    skilcard 중간 저장

--- src/Card/SkilCard.js
+++ src/Card/SkilCard.js
@@ -1,23 +1,56 @@
 import React from "react";
-import styled from "styled-components";
-import { PanResponder, Animated } from "react-panresponder-web";
-
-const SkilContainer = styled.article`
+import styled, { keyframes } from "styled-components";
+import { useState } from "react";
+import { flipInY, fadeInLeft } from "react-animations";
+const cardIn = keyframes`${fadeInLeft}`;
+const SkilContainer = styled.div`
+  width: 100%;
+  height: 96vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const SkilArticle = styled.article`
   position: absolute;
-  width: 23%;
+  width: 18%;
   height: 60vh;
   background-color: red;
-`;
-const Title = styled.h3``;
 
+  animation: ${cardIn} 0.5 ease-in-out;
+`;
+const LeftBtn = styled.button`
+  width: 100px;
+  height: 50px;
+  z-index: 222;
+`;
+const RightBtn = styled.button`
+  z-index: 222;
+`;
 function SkilCard({ skils }) {
   console.log(skils);
+  const [current, setCurrent] = useState(0);
+
   return (
-    <>
-      {skils.map((item) => (
-        <SkilContainer></SkilContainer>
-      ))}
-    </>
+    <SkilContainer>
+      {skils
+        .map((item, index) => (
+          <>
+            {current === index && (
+              <>
+                <SkilArticle className={index === current && "topCard"}>
+                  <div> {item.title}</div>
+                  {index}
+                </SkilArticle>
+                <LeftBtn onClick={() => setCurrent(current - 1)}>이전</LeftBtn>
+                <RightBtn onClick={() => setCurrent(current + 1)}>
+                  다음
+                </RightBtn>
+              </>
+            )}
+          </>
+        ))
+        .reverse()}
+    </SkilContainer>
   );
 }
 
