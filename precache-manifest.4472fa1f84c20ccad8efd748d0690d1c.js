55a561710d26a7f432490fed9d0532c2d4d30ebb
SangMinL96 <59109788+SangMinL96@users.noreply.github.com>
2020-08-30 22:03:46 +0900

    Updates

---
+++ precache-manifest.4472fa1f84c20ccad8efd748d0690d1c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dcc7b5d3627c78974bdfab062896c89",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "5321d91cde7d1a5474ab",
+    "url": "/react-portfolio/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "5321d91cde7d1a5474ab",
+    "url": "/react-portfolio/static/js/2.eb970344.chunk.js"
+  },
+  {
+    "revision": "afb26294e97ca93182879cc39a230f60",
+    "url": "/react-portfolio/static/js/2.eb970344.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bee4bfcb0a0e7be832f",
+    "url": "/react-portfolio/static/js/main.0647e5f4.chunk.js"
+  },
+  {
+    "revision": "c34c353c004898710743",
+    "url": "/react-portfolio/static/js/runtime-main.13d31e70.js"
+  },
+  {
+    "revision": "fd068586e170ec4ecdab3d2840f3c9fc",
+    "url": "/react-portfolio/static/media/ProFileimg.fd068586.jpg"
+  },
+  {
+    "revision": "a1ad8e73269f126af1458842f0aa3f20",
+    "url": "/react-portfolio/static/media/alba.a1ad8e73.PNG"
+  },
+  {
+    "revision": "9c53358678779fbfb93d57bfa13f3018",
+    "url": "/react-portfolio/static/media/albaimg.9c533586.PNG"
+  },
+  {
+    "revision": "10cf1e591b2328f2d417598c1cb7512d",
+    "url": "/react-portfolio/static/media/army.10cf1e59.jpg"
+  },
+  {
+    "revision": "a285d94725a17b3826b9258d8b5bbd52",
+    "url": "/react-portfolio/static/media/bg.a285d947.jpg"
+  },
+  {
+    "revision": "13144bad59b6b91e74a4b05053eb2fdc",
+    "url": "/react-portfolio/static/media/homelogo.13144bad.png"
+  },
+  {
+    "revision": "fa88a919142d8266ed53f15d9b1cd717",
+    "url": "/react-portfolio/static/media/mark.fa88a919.png"
+  },
+  {
+    "revision": "d88a3d22ec98331b8842c9a1704b482e",
+    "url": "/react-portfolio/static/media/myImg1.d88a3d22.jpg"
+  },
+  {
+    "revision": "14cccd22f7d7c7f2b6c0b660b5c3e425",
+    "url": "/react-portfolio/static/media/myimg.14cccd22.png"
+  },
+  {
+    "revision": "b839b1f677a0498dbe844cac2f7a1c5a",
+    "url": "/react-portfolio/static/media/school.b839b1f6.png"
+  },
+  {
+    "revision": "3c6afa347b9cbd1bc05704b98f810b74",
+    "url": "/react-portfolio/static/media/schoolbg.3c6afa34.jpg"
+  },
+  {
+    "revision": "cf3c512b58ed5c16f73d5c55a8912fd6",
+    "url": "/react-portfolio/static/media/textimg.cf3c512b.png"
+  }
+]);
